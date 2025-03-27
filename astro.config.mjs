80a1a4a09d5c32e58cb3e145375350148ee16d3b
shaun.michael107 <shaun.michael107@gmail.com>
2025-03-27 20:27:15 +0000

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://shahun-107.github.io',
+  base: 'testappsvz',
   integrations: [mdx(), sitemap()],
 
   vite: {
